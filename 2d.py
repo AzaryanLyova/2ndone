27438835b76eb4fd01ebe3e401db08b6d633beb8
AzaryanLyova <lyova.azaryan.y@gmail.com>
2022-11-23 17:56:42 +0400

    thisIsTheSecondOneYet

--- 2d.py
+++ 2d.py
@@ -4,3 +4,8 @@ print('HellFBFSB52e ')
 print('Hello worldFSB29Bf9F for the 2nd time ')
 print('Hell925gfn292xfgn ')
 print('Hefxhn925xf2ngime ')
+
+print('How are your kfdlbn')
+print(548585)
+print(65153+62)
+print('IDK')
